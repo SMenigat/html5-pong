3cbc846abae6a3be148eae75e49307fbe5cdea0c
Samuel Menigat <smenigat@samuels-mbp.speedport_w_724v_typ_a_05011603_00_007>
2016-01-18 22:25:43 +0100

    first running version of the game (still very raw)

--- pong.js
+++ pong.js
@@ -8,9 +8,12 @@
 var PongGame = function(gameCanvasNodeId, Player1Name, Player2Name){
     var self = this;
 
-    // game performance settings
+    // game performance settings & timings
     var drawRate = 10; // in milliseconds
     var gameSpeed = 2;
+    var gameBounceSmoothness = 0.05;
+    var gameBounceSpeedRatio = 0.25;
+    var scorePauseTimer = 1000; // in milliseconds
 
     // trigger that stops the game
     var gameStopped = false;
@@ -52,13 +55,14 @@ var PongGame = function(gameCanvasNodeId, Player1Name, Player2Name){
     var scoreFinal = 10;
     var scorePlayer1 = 0;
     var scorePlayer2 = 0;
+    var isMultiplayer = true;
 
     // input control's
-    var keyboarIO = {
+    self.keyboardIO = {
         Player1Up: false,
         Player1Down: false,
-        Player1Up: false,
-        Player1Down: false
+        Player2Up: false,
+        Player2Down: false
     };
 
     /**
@@ -80,11 +84,22 @@ var PongGame = function(gameCanvasNodeId, Player1Name, Player2Name){
             canvasHeight / 2
         );
 
+        // register event hanlder for keyboard inputs
+        document.addEventListener('keypress', keyboardIOHandler);
+        document.addEventListener('keyup', keyboardIOReset);
+
         // create the ball game object & give it a random velocity and direction
         resetBall();
 
         // start the game engine
         engine();
+    };
+
+    /**
+     * method stops the game engine
+     */
+    this.stop = function(){
+        gameStopped = true;
     };
 
     /**
@@ -96,7 +111,70 @@ var PongGame = function(gameCanvasNodeId, Player1Name, Player2Name){
         if (gameStopped) return;
 
         // move the players bats
+        if (self.keyboardIO.Player1Up && canBatBeMovedOnYAxis(Player1Bat, (gameSpeed * 2) * -1)) {
+            Player1Bat.moveObject(new Vector(0, (gameSpeed * 2) * -1));
+        }
+        if (self.keyboardIO.Player1Down && canBatBeMovedOnYAxis(Player1Bat, (gameSpeed * 2))) {
+            Player1Bat.moveObject(new Vector(0, (gameSpeed * 2)));
+        }
+        if (self.keyboardIO.Player2Up && canBatBeMovedOnYAxis(Player2Bat, (gameSpeed * 2) * -1)) {
+            Player2Bat.moveObject(new Vector(0, (gameSpeed * 2) * -1));
+        }
+        if (self.keyboardIO.Player2Down && canBatBeMovedOnYAxis(Player2Bat, (gameSpeed * 2))) {
+            Player2Bat.moveObject(new Vector(0, (gameSpeed * 2)));
+        }
 
+        // check if ball colides with player1's bat
+        if (checkBallCollision(Player1Bat)) {
+
+            // we need to add a velocity limit to avoid collision detection from bugging out
+            var VelocityX = (BallVelocity.x - (gameSpeed * gameBounceSpeedRatio));
+            if (Math.abs(VelocityX) > ballRadius) {
+                VelocityX = ballRadius * -1;
+            }
+
+            // bounce the ball from the bat & speed the ball up
+            BallVelocity = new Vector(
+                VelocityX * -1,
+                (Player1Bat.y - Ball.y) * gameBounceSmoothness * -1
+            );
+        }
+
+        // check if ball colides with player2's bat
+        if (checkBallCollision(Player2Bat)) {
+
+            // we need to add a velocity limit to avoid collision detection from bugging out
+            var VelocityX = (BallVelocity.x + (gameSpeed * gameBounceSpeedRatio));
+            if (Math.abs(VelocityX) > ballRadius) {
+                VelocityX = ballRadius;
+            }
+
+            // bounce the ball from the bat & speed the ball up
+            BallVelocity = new Vector(
+                VelocityX * -1,
+                (Player2Bat.y - Ball.y) * gameBounceSmoothness * -1
+            );
+        }
+
+        // check if ball collides with canvas's top border
+        if ((Ball.y - ballRadius) <= 0) {
+
+            // bounce the ball
+            BallVelocity = new Vector(
+                BallVelocity.x,
+                BallVelocity.y * -1
+            );
+        }
+
+        // check if ball collides with canvas's bottom border
+        if ((Ball.y + ballRadius) >= canvasHeight) {
+
+            // bounce the ball
+            BallVelocity = new Vector(
+                BallVelocity.x,
+                BallVelocity.y * -1
+            );
+        }
 
         // move the ball
         Ball.moveObject(BallVelocity);
@@ -154,6 +232,51 @@ var PongGame = function(gameCanvasNodeId, Player1Name, Player2Name){
     };
 
     /**
+     * checks if ball colides with given player bat
+     * @param playerBat
+     * @returns {boolean}
+     */
+    var checkBallCollision = function(playerBat) {
+
+        // bat coordinates
+        var batInfo = {
+            centerX: playerBat.x,
+            centerY: playerBat.y,
+            smallestX: playerBat.x - (batWidth / 2),
+            biggestX: playerBat.x + (batWidth / 2),
+            smallestY: playerBat.y - (batHeight / 2),
+            biggestY: playerBat.y + (batHeight / 2)
+        };
+
+        // ball coordinates
+        var ballInfo = {
+            centerX: Ball.x,
+            centerY: Ball.y,
+            smallestX: Ball.x - ballRadius,
+            biggestX: Ball.x + ballRadius,
+            smallestY: Ball.y - ballRadius,
+            biggestY: Ball.y + ballRadius
+        };
+
+        // check if they would collide on the x axis
+        if (
+            (batInfo.smallestX <= ballInfo.smallestX && ballInfo.smallestX <= batInfo.biggestX) ||
+            (batInfo.smallestX <= ballInfo.biggestX && ballInfo.biggestX <= batInfo.biggestX)
+        ) {
+            // check if the are colliding on the y axis too
+            if (
+                (batInfo.smallestY <= ballInfo.smallestY && ballInfo.smallestY <= batInfo.biggestY) ||
+                (batInfo.smallestY <= ballInfo.biggestY && ballInfo.biggestY <= batInfo.biggestY)
+            ) {
+                return true;
+            }
+        }
+
+        // the ball didn't collide with the given bat
+        return false;
+    };
+
+    /**
      * (re)creates the ball game object and generates a random velocity for it
      */
     var resetBall = function(){
@@ -171,8 +294,101 @@ var PongGame = function(gameCanvasNodeId, Player1Name, Player2Name){
         } else {
             BallVelocity = new Vector(gameSpeed, 0);
         }
+
+        // we pause the script execution for the defined pause time
+        var startTime = new Date();
+        var currentTime = null;
+        do {
+            currentTime = new Date();
+        } while(currentTime - startTime < scorePauseTimer);
     };
 
+    /**
+     * checks if bat can be moved up or down the Y axis, or if bat is already to close to the border
+     * @param bat
+     * @param distance
+     * @returns {boolean}
+     */
+    var canBatBeMovedOnYAxis = function(bat, distance) {
+
+        // are we to far too the top if we move the bat?
+        if ((bat.y - (batHeight / 2)) + distance <= 0) {
+            console.log('bat too far up', (bat.y - (batHeight / 2)) + distance);
+            return false;
+        }
+
+        // are we too far to the bottom if we move the bat?
+        if ((bat.y + (batHeight / 2)) + distance >= canvasHeight) {
+            console.log('bat too far down', (bat.y + (batHeight / 2)) + distance);
+            return false;
+        }
+
+        // everything is fine, bat can be moved
+        return true;
+    };
+
+    /**
+     * eventhandler for keypress events of document which sets the player's io states
+     * @param event
+     */
+    var keyboardIOHandler = function(event){
+
+        // detect pressed char code
+        var characterCode = (typeof event.which == "number") ? event.which : event.keyCode;
+
+        // if player1 moves the bat
+        if (characterCode == 119 || characterCode == 115) {
+
+            // up
+            if (characterCode == 119) {
+                self.keyboardIO.Player1Up = true;
+                self.keyboardIO.Player1Down = false;
+            }
+
+            // down
+            else if (characterCode == 115) {
+                self.keyboardIO.Player1Up = false;
+                self.keyboardIO.Player1Down = true;
+            }
+        } else {
+
+            // player1 is not moving
+            self.keyboardIO.Player1Up = false;
+            self.keyboardIO.Player1Down = false;
+        }
+
+        // if player1 moves the bat
+        if (characterCode == 111 || characterCode == 108) {
+
+            // up
+            if (characterCode == 111) {
+                self.keyboardIO.Player2Up = true;
+                self.keyboardIO.Player2Down = false;
+            }
+
+            // down
+            else if (characterCode == 108) {
+                self.keyboardIO.Player2Up = false;
+                self.keyboardIO.Player2Down = true;
+            }
+        } else {
+
+            // player1 is not moving
+            self.keyboardIO.Player2Up = false;
+            self.keyboardIO.Player2Down = false;
+        }
+    };
+
+    /**
+     * resets the keyboard input events
+     * @param event
+     */
+    var keyboardIOReset = function(event) {
+        self.keyboardIO.Player1Up = false;
+        self.keyboardIO.Player1Down = false;
+        self.keyboardIO.Player2Up = false;
+        self.keyboardIO.Player2Down = false;
+    };
 
     /**
      * writes the winners name & reset information centered onto the canvas
